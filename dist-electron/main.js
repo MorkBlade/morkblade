6fe632cdc89653882e1fac50eb37707cb9104bf4
tangbin <631297081@qq.com>
2025-07-05 09:26:52 +0800

    fix: 更换应用图标
    Former-commit-id: 55405bea45f7bf55519e3d0c38090dcee5aabb9d

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,79 +1,52 @@
-import { app, BrowserWindow } from "electron";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
-let win;
-const createWindow = () => {
-  win = new BrowserWindow({
+import { app as s, BrowserWindow as a } from "electron";
+import r from "node:path";
+import { fileURLToPath as u } from "node:url";
+const v = u(import.meta.url), d = r.dirname(v);
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
+let e;
+const c = () => {
+  e = new a({
     width: 1920,
     height: 1080,
     //frame: false,// 去掉窗口边框
     //transparent: true,// 允许窗口透明
     //title: '星闪悦动键盘量产工具',
     title: "MORKBLADE",
-    icon: path.join(__dirname, "../public/logo.ico"),
+    icon: r.join(d, "../public/logo.ico"),
     webPreferences: {
       //preload: path.join(__dirname, 'preload.js'),
-      devTools: true,
+      devTools: !0,
       // 是否打开调试模式
-      webSecurity: false,
+      webSecurity: !1,
       // 禁用安全策略
-      allowDisplayingInsecureContent: true,
+      allowDisplayingInsecureContent: !0,
       // 允许一个使用 https的界面来展示由 http URLs 传过来的资源
-      allowRunningInsecureContent: true,
+      allowRunningInsecureContent: !0,
       // 允许一个 https 页面运行 http url 里的资源
-      nodeIntegration: true,
+      nodeIntegration: !0,
       // 5.x以上版本，默认无法在渲染进程引入node模块，需要这里设置为true  //是否集成node，默认false
-      enableRemoteModule: true,
+      enableRemoteModule: !0,
       // 打开remote模块
-      contextIsolation: false
+      contextIsolation: !1
     }
-  });
-  win.setMenu(null);
-  win.webContents.on("console-message", (event, level, message) => {
-    if (message.includes("Autofill.enable") || message.includes("Autofill.setAddresses")) {
-      event.preventDefault();
+  }), e.setMenu(null), e.webContents.on("console-message", (o, n, t) => {
+    if (t.includes("Autofill.enable") || t.includes("Autofill.setAddresses")) {
+      o.preventDefault();
       return;
     }
-  });
-  win.webContents.session.on("select-hid-device", (event, details, callback) => {
-    console.log("select-hid-device");
-    win.webContents.session.on("hid-device-added", (event2, device) => {
-      console.log("hid-device-added FIRED WITH", device);
-    });
-    win.webContents.session.on("hid-device-removed", (event2, device) => {
-      console.log("hid-device-removed FIRED WITH", device);
-    });
-    event.preventDefault();
-    console.log(details);
-    if (details.deviceList && details.deviceList.length > 0) {
-      console.log(details);
-      callback(details.deviceList[0].deviceId);
-    }
-  });
-  win.webContents.session.setPermissionCheckHandler((webContents, permission, requestingOrigin, details) => {
-    console.log("setPermissionCheckHandler", details);
-    return true;
-  });
-  win.webContents.session.setDevicePermissionHandler((details) => {
-    console.log("setDevicePermissionHandler", details);
-    return true;
-  });
-  if (process.env.VITE_DEV_SERVER_URL) {
-    win.loadURL(process.env.VITE_DEV_SERVER_URL);
-    win.webContents.openDevTools();
-  } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
-  }
+  }), e.webContents.session.on("select-hid-device", (o, n, t) => {
+    console.log("select-hid-device"), e.webContents.session.on("hid-device-added", (i, l) => {
+      console.log("hid-device-added FIRED WITH", l);
+    }), e.webContents.session.on("hid-device-removed", (i, l) => {
+      console.log("hid-device-removed FIRED WITH", l);
+    }), o.preventDefault(), console.log(n), n.deviceList && n.deviceList.length > 0 && (console.log(n), t(n.deviceList[0].deviceId));
+  }), e.webContents.session.setPermissionCheckHandler((o, n, t, i) => (console.log("setPermissionCheckHandler", i), !0)), e.webContents.session.setDevicePermissionHandler((o) => (console.log("setDevicePermissionHandler", o), !0)), process.env.VITE_DEV_SERVER_URL ? (e.loadURL(process.env.VITE_DEV_SERVER_URL), e.webContents.openDevTools()) : e.loadFile(r.join(d, "../dist/index.html"));
 };
-app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+s.whenReady().then(() => {
+  c(), s.on("activate", () => {
+    a.getAllWindows().length === 0 && c();
   });
 });
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+s.on("window-all-closed", () => {
+  process.platform !== "darwin" && s.quit();
 });
